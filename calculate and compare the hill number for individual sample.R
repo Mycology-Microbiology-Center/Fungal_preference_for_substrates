69da3cf35375fb95798e8ab4308a2f1834c1990b
meirongchen17 <144226739+meirongchen17@users.noreply.github.com>
2026-01-04 21:56:26 +0200

    Rename file to calculate hill number for samples

--- calculate and compare the hill number for individual sample.R
+++ calculate and compare the hill number for individual sample.R
@@ -1,7 +1,10 @@
 library(BAT)
-setwd("C:/Users/meirong/Desktop/PhD project/second preference/final.table/third/1.calculate the result/")
+library(rcompanion)
+library(dplyr)
+library(ggplot2)
+library(lmerTest)
+library(marginaleffects)
 metadata<-read.csv("metadata.final2.csv",row.names = 1)
-setwd("C:/Users/meirong/Desktop/PhD project/second preference/final.table/organised code/table/")
 fungi<-read.csv("table.nolichenandhost.csv",row.names = 1)
 fungi<-as.data.frame(t(fungi))
 r<-hill(fungi, q = 1, raref = 0, runs = 999)
@@ -10,17 +13,11 @@ r2$sample_names<-row.names(r2)
 r2<-merge(r2,metadata,by="sample_names",all=T)
 r2<-r2[!is.na(r2$`Hill 1`),]
 names(r2)[2]<-'hill.1'
-##
-##
-library(lmerTest)
-library(marginaleffects)
 model <- lm(hill.1 ~  substrate + site2, data = r2)
 ###
 b<-avg_comparisons(model, variables = list(substrate = "pairwise")) 
 performance::performance(model)
 substrate.richness<-parameters::model_parameters(model)
-library(rcompanion)
-library(dplyr)
 letter<-cldList(p.value~contrast,threshold = 0.05,data=b)
 letter$Group[letter$Group == "fruitbody"] <- "fruit body"
 letter$Group[letter$Group == "leaflitter"] <- "leaf litter"
@@ -30,8 +27,6 @@ difference <- r2 %>%
 
 y.site<-merge(letter,difference,by.x="Group",by.y="substrate")
 y.site<-data.frame(substrate=factor(y.site$Group),ymax=y.site$max,letter=y.site$Letter)
-library(ggplot2)
-
 plot_predictions(model,condition = c("substrate"))+
   labs(x="substrate",y="hill number")+
   theme_light() +
@@ -45,7 +40,6 @@ plot_predictions(model,condition = c("substrate"))+
     axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
     legend.position = "bottom")+
   geom_text(data = y.site, aes(x = substrate , y = ymax, label = letter,hjust=-0.5))
-
 write.csv(substrate.richness,"q1.richnessparameter.csv")
 ###
 r<-hill(fungi, q = 0, raref = 0, runs = 999)
@@ -57,7 +51,6 @@ names(r2)[2]<-'hill.0'
 write.csv(r2,"richness.csv")
 ##
 model <- lm(hill.0 ~  substrate + site2, data = r2)
-###
 b<-avg_comparisons(model, variables = list(substrate = "pairwise")) 
 performance::performance(model)
 # options(scipen = 999)
@@ -84,3 +77,4 @@ plot_predictions(model,condition = c("substrate"))+
     legend.position = "bottom")+
   geom_text(data = y.site, aes(x = substrate , y = ymax, label = letter,hjust=-0.5))
 write.csv(substrate.richness,"q0.richnessparameter.csv")
+
