98c96c32c1674e69677e706f74c8a8c065ea12b0
meirongchen17 <144226739+meirongchen17@users.noreply.github.com>
2026-01-04 21:41:04 +0200

    Update find the dominant order.R

--- find the dominant order.R
+++ find the dominant order.R
@@ -2,9 +2,7 @@ library(adegenet)
 library(tidyverse)
 library(gridExtra)
 library(dplyr)
-setwd("C:/Users/meirong/Desktop/PhD project/second preference/final.table/third/1.calculate the result/")
 metadata<-read.csv("metadata.final2.csv",row.names = 1)
-setwd("C:/Users/meirong/Desktop/PhD project/second preference/final.table/organised code/table/")
 fungi<-read.csv("table.nolichenandhost.csv",row.names = 1)
 table<-as.data.frame(t(fungi))
 table2<-table
@@ -12,7 +10,6 @@ table2$sample_names<-row.names(table2)
 table2<-merge(table2,metadata,by="sample_names")
 metadata <- metadata[match(row.names(table),metadata$sample_names),]
 ###
-setwd("C:/Users/meirong/Desktop/PhD project/second preference/final.table/forth/7.living type")
 tax<-read.csv("taxonomy.final.csv")
 ###
 table3<-as.data.frame(table2)
@@ -30,3 +27,4 @@ try<-ge
 try<- try %>%
   group_by(substrate,order,site) %>%
   summarise(Ratio=sum(Ratio))
+
